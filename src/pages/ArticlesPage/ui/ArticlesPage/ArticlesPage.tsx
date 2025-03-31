0fbdee6f8b7a5aa54c789dcea89f8c9cf2bd2097
PavlovichVladislav <jeffry2814@gmail.com>
2025-03-31 23:39:11 +0700

    ArticlesPage | provide loading and view from state

--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,12 +1,17 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import { memo } from 'react';
-import { ArticleList, ArticleView } from 'entities/Article';
+import { ArticleList } from 'entities/Article';
 import { DynamicModuleLoader, ReducersList } from 'shared/ui/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch';
-import { fetchArticleList } from 'pages/ArticlesPage/model/services/fetchArticleList/fetchArticleList';
 import { useSelector } from 'react-redux';
 import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect';
+import { fetchArticleList } from '../../model/services/fetchArticleList/fetchArticleList';
+import {
+  getArticlesPageError,
+  getArticlesPageIsLoading,
+  getArticlesPageView,
+} from '../../model/selectors/articlePageSelectors';
 import { articlesPageReducer, getArticles } from '../../model/slices/articlesPageSlice';
 import styles from './ArticlesPage.module.scss';
 
@@ -21,6 +26,9 @@ const reducers: ReducersList = {
 const ArticlesPage = ({ className }: ArticlesPageProps) => {
   const articles = useSelector(getArticles.selectAll);
   const dispatch = useAppDispatch();
+  const isLoading = useSelector(getArticlesPageIsLoading);
+  const view = useSelector(getArticlesPageView);
+  const error = useSelector(getArticlesPageError);
 
   useInitialEffect(() => {
     dispatch(fetchArticleList());
@@ -30,7 +38,7 @@ const ArticlesPage = ({ className }: ArticlesPageProps) => {
     <DynamicModuleLoader reducers={reducers} reducerName="articlesPage">
       <div className={classNames(styles.articlesPage, {}, [className])}>
         {/* @ts-ignore */}
-        <ArticleList isLoading={false} articles={articles} view={ArticleView.LIST} />
+        <ArticleList isLoading={isLoading} articles={articles} view={view} />
       </div>
     </DynamicModuleLoader>
   );
